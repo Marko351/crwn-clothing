84a397c7ab740308e6e06d1800300303755e9d40
Marko351 <markodjuric351@gmail.com>
2019-08-24 12:50:11 +0200

    Initial commit. Added homepage and manu items

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory';
+
+import './Homepage.scss';
+
+const Homepage = () => {
+  return (
+    <div classname='Homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
