0162d821b024fd830bd06a5c79c2ca14970f33bc
Marko351 <markodjuric351@gmail.com>
2019-08-24 16:41:08 +0200

    updating app with shop page, collection previev and item component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/Homepage/Homepage';
+import ShopPage from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact to='/' component={Homepage} />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
